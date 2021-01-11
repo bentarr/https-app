6e792970d960964d250c7b639cf9622dd406ab95
bentarr <benetierpaul@gmail.com>
2021-01-11 15:38:02 +0100

    Création serveur + communication fichier html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
+app.use(express.static('public'));
+
+app.listen(80,() => console.log("Server écoute sur port 80"));
