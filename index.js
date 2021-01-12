ee5af264fc0f20b816905463114f0b1468ab5b77
bentarr <benetierpaul@gmail.com>
2021-01-12 14:31:00 +0100

    Avancée dans https

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const path = require ('path')
+
 const app = express()
+const fichierServer = 'client'
+const port = 3443
 
-app.use(express.static('public'));
 
-app.listen(80,() => console.log("Server écoute sur port 80"));
+//app.use(express.static('public'));
+
+app.use('/',express.static(path.join(__dirname,'..',fichierServer)))
+
+const httpsOptions = {
+    cert: fs.readFileSync(path.join(__dirname,'ssl', 'server.crt')),
+    key: fs.readFileSync(path.join(__dirname,'ssl', 'server.key'))
+}
